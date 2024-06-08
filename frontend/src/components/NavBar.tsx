6c732773887ed5def0a0d189b2fef9208a07d44f
Zekarias Mesfin <zekariasmesfin100@gmail.com>
2024-06-08 19:11:30 +0300

    corrected the navbar-brand link

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Link from 'next/link';
 
 const Navbar: React.FC = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
       <div className="container">
-        <a className="navbar-brand" href="#page-top">Zekarias Mesfin</a>
+        <Link className="navbar-brand" href="/">Zekarias Mesfin</Link>
         <button className="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           Menu
           <i className="fas fa-bars"></i>
