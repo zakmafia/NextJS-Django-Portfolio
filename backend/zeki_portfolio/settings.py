defd63d06902eac37feaa6fe58ced31d0d29bc5a
Zekarias Mesfin <zekariasmesfin100@gmail.com>
2024-06-14 10:01:49 +0300

    .env configuration

--- backend/zeki_portfolio/settings.py
+++ backend/zeki_portfolio/settings.py
@@ -1,5 +1,8 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,10 +12,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^q8(=9e-kv8g$sz&068u&2tow=s$lb+jn0_*-1nj*e*(58ogy$'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG")
 
 ALLOWED_HOSTS = []
 
